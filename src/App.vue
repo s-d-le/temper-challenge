417dff27ab8aaabd9ceac90cc5643fb624e3207c
Son Le <ayzkeys@gmail.com>
2024-04-04 23:03:45 +0700

    core logic seems to work

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 import Post from "./components/Post.vue";
 
 type Post = {
@@ -8,6 +7,8 @@ type Post = {
 };
 
 const posts = ref<Post[]>([]);
+
+const history = ref<{ text: string; index: number; state: number[] }[]>([]);
 
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then((response) => response.json())
@@ -20,26 +21,49 @@ const movePost = (index: number, direction: number) => {
   const movedPost = posts.value[index];
   const newIndex = index + direction;
 
+  //remove the post from the array
   posts.value.splice(index, 1);
+  //insert the post at the new index
   posts.value.splice(newIndex, 0, movedPost);
 
-  // this.history.push({
-  //   text: `Moved Post ${movedPost.id} from index ${index} to index ${newIndex}`,
-  //   index: newIndex,
-  // });
-  console.log(index, posts.value);
+  //history of the action
+  history.value.push({
+    text: `Moved Post ${movedPost.id} from index ${index} to index ${newIndex}`,
+    index: newIndex,
+    state: posts.value.map((post) => post.id), //save the state of the posts
+  });
+
+  console.log(index, history.value);
+};
+
+const goBack = (index: number) => {
+  const targetState = history.value[index].state;
+  posts.value = targetState.map(
+    (id) => posts.value.find((post) => post.id === id)!
+  );
+  history.value.splice(index + 1);
 };
 </script>
 
 <template>
-  <Post
-    v-for="post in posts"
-    :key="post.id"
-    :id="post.id"
-    @click:move-down="movePost(post.id, 1)"
-    @click:move-up="movePost(post.id, -1)"
-  />
-  <HelloWorld msg="Vite + Vue" />
+  <div>
+    <Post
+      v-for="(post, index) in posts"
+      :key="post.id"
+      :id="post.id"
+      @click:move-down="movePost(index, 1)"
+      @click:move-up="movePost(index, -1)"
+    />
+  </div>
+  <div>
+    <h2>History</h2>
+    <ul>
+      <li v-for="(item, index) in history" :key="item.index">
+        {{ item.text }}
+        <button @click="goBack(index)">Go back here</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped></style>
